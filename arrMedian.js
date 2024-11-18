50fe917fb5313a54ea39cf6b894913d17d7fe9c7
Priyam Mondal <mondalpriyam99@gmail.com>
2024-11-18 20:41:11 +0700

    test first commit and push

---
+++ arrMedian.js
@@ -0,0 +1,21 @@
+
+function median(arr){
+    
+    arr.sort((a,b) => a-b);
+    
+    const len=arr.length;
+    const mid=Math.floor(len/2);
+    
+    if(len%2!=0){
+        return arr[mid];
+    }else{
+        return (arr[mid-1]+arr[mid])/2;
+    }
+    
+}
+
+let arr1=[5, 3, 8, 1, 2, 4];
+let arr2=[1,2,-3,-4,5];
+
+console.log(median(arr1));
+console.log(median(arr2));
